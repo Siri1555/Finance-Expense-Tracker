f78ab3518bc854aba9fe6c5503565fe1426c79d7
Siri1555 <113041019+Siri1555@users.noreply.github.com>
2024-05-21 23:39:00 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,99 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+// const hbs = require("hbs")
+const LogInCollection = require("./mongo")
+const port = process.env.PORT || 3000
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
+
+const tempelatePath = path.join(__dirname, '../tempelates')
+const publicPath = path.join(__dirname, '../public')
+console.log(publicPath);
+
+app.set('view engine', 'hbs')
+app.set('views', tempelatePath)
+app.use(express.static(publicPath))
+
+
+// hbs.registerPartials(partialPath)
+
+
+app.get('/signup', (req, res) => {
+    res.render('signup')
+})
+app.get('/', (req, res) => {
+    res.render('login')
+})
+
+
+
+// app.get('/home', (req, res) => {
+//     res.render('home')
+// })
+
+app.post('/signup', async (req, res) => {
+    
+    // const data = new LogInCollection({
+    //     name: req.body.name,
+    //     password: req.body.password
+    // })
+    // await data.save()
+    let {name,password} = req.body;
+    console.log(name,password);
+    const data = req.body;
+    // console.log(data);
+
+    const checking = await LogInCollection.findOne({ name });
+    console.log(checking)
+
+   try{
+    if ((checking!=null) && checking.name === name && checking.password===password) {
+        res.send("user details already exists")
+    }
+    else{
+        await LogInCollection.insertMany([data]);
+    }
+   }
+   catch(err){
+    // res.send("wrong inputs",err)
+    console.log("Error Occured: ",err);
+   }
+
+    res.status(201).render("login", {
+        naming: req.body.name
+    })
+})
+
+
+app.post('/login', async (req, res) => {
+
+    try {
+        const check = await LogInCollection.findOne({ name: req.body.name })
+
+        if (check.password === req.body.password) {
+            res.status(201).render("home", { name: req.body.name })        }
+
+        else {
+            res.send("incorrect password")
+        }
+
+
+    } 
+    
+    catch (e) {
+
+        res.send("wrong details")
+        
+
+    }
+
+
+})
+
+
+
+app.listen(port, () => {
+    console.log('port connected');
+})
